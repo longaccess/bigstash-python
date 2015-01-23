625877e195eb7c8c462a7e4f54a07291d9fdb622
Sofia Margariti <sofiamargariti@hotmail.com>
2015-01-23 15:57:48 +0200

    Update auth head for both basic and signature authentication

--- robohydra/plugins/bigstashauth/index.js
+++ robohydra/plugins/bigstashauth/index.js
@@ -1,6 +1,8 @@
 var heads               = require('robohydra').heads,
     RoboHydraHead       = require("robohydra").heads.RoboHydraHead,
+    RoboHydraHeadStatic = heads.RoboHydraHeadStatic, 
     apiPrefix           = require("./index.js").apiPrefix
+
 
 exports.getBodyParts = function(config, modules) {
     var token = {
@@ -13,57 +15,60 @@ exports.getBodyParts = function(config, modules) {
     return {
         heads: [
             new RoboHydraHead({
-                path: '/api/v1/tokens',
+                path: '/.*',
                 handler: function(req, res, next) {
-                    modules.assert.equal(req.method, "POST", "Token resource supports only POST")
                     if("authorization" in req.headers) {
-                        var auth = req.headers["authorization"].split(" ");
-                        modules.assert.equal(auth.length, 2, "client sent invalid auth");
-                        modules.assert.equal(auth[0], "Basic", "client didn't send basic auth");
-                        var b64creds = new Buffer(auth[1], 'base64'),
-                            creds = b64creds.toString().split(":");
-                        modules.assert.equal(creds.length, 2, "client sent invalid basic auth");
-                        if (creds[0] == "test" && creds[1] == "test") {
-                            res.statusCode = '200';
-                            res.send(JSON.stringify(token));
-                        } else {
-                            res.authfail = true
+                        var auth = req.headers["authorization"];
+                        if (auth.indexOf("Signature") == 0){
+                            var start = "Signature ";
+                            var parts = auth.slice(start.length);
+                            parts = parts.split(', ');
+                            modules.assert.equal(parts[0].indexOf('keyId'), 0, "Missing keyId from signature");
+                            modules.assert.equal(parts[0].indexOf('algorithm'), 0, "Missing algorithm form signature");
+                            modules.assert.equal(parts[0].indexOf('headers'), 0, "Missing headers from signature");
+                            modules.assert.equal(parts[0].indexOf('signature'), 0, "Missing signature from signature");
+                            res.authuser = true;
+                            return next(req, res);
+                        } else if ((auth.indexOf("Basic") == 0) && (req.url == '/api/v1/tokens/')){
+                            var parts = auth.split(" ");
+                            modules.assert.equal(auth.length, 2, "client sent invalid auth");
+                            modules.assert.equal(auth[0], "Basic", "client didn't send basic auth");
+                            var b64creds = new Buffer(parts[1], 'base64'),
+                                creds = b64creds.toString().split(":");
+                            modules.assert.equal(creds.length, 2, "client sent invalid basic auth");
+                            if (creds[0] == "test" && creds[1] == "test") {
+                                res.authuser = true;
+                                return next(req, res);
+                            } else {
+                                res.authfail = true;
+                            } 
                         }
+                    } else {
+                        res.authfail = true
+                    }
+                    if (res.hasOwnProperty('authuser')) {
+                        res.authfail = true
                     }
                     if (res.hasOwnProperty('authfail')) {
                         res.headers["www-authenticate"] = 'Basic realm="foobar"';
                         res.statusCode = '401';
-                        res.send("401 - Forbidden")
+                        res.send("401 - Forbidden");
+                    } else {
+                        return next(req, res)
                     }
-                    res.end();
                 }
+            }),
+            new RoboHydraHeadStatic({
+                path: '/api/v1/tokens/',
+                content: token
             })
-//            new RoboHydraHead({
-//                path: '/api/v1/test',
-//                handler: function(req, res, next) {
-//                    if("authorization" in req.headers) {
-//                        var auth = req.headers["authorization"];
-//                        modules.assert.ok('KeyId' in auth, "KeyId missing from signature auth header");
-//                        modules.assert.ok('algorithm' in auth, "algorithm missing from signature auth header");
-//                        modules.assert.ok('headers' in auth, "headers missing from signature auth header");
-//                        modules.assert.ok('signature' in auth, "signature missing from signature auth header");
-//                    }
-//                    if (res.hasOwnProperty('authfail')) {
-//                        res.headers["www-authenticate"] = 'Basic realm="foobar"';
-//                        res.statusCode = '401';
-//                        res.send("401 - Forbidden")
-//                    } else {
-//                        return next(req, res)
-//                    }
-//                }
-//            })
         ],
         scenarios: {
             authFails: {
                 instructions: "Authentication will fail",
                 heads: [
                     new RoboHydraHead({
-                        path: '/.*',
+                        path: '/tokens/',
                         handler: function(req, res, next) {
                             res.authfail = true;
                             next(req, res);
@@ -75,7 +80,7 @@ exports.getBodyParts = function(config, modules) {
                 instructions: "Authentication for user test with password test",
                 heads: [
                     new RoboHydraHead({
-                        path: '/.*',
+                        path: '/tokens/',
                         handler: function(req, res, next) {
                             res.authuser = true;
                             next(req, res);
