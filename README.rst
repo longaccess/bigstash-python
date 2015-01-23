b0941abbe96877634a1b7d9c688323873a06ef25
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-01-23 18:39:10 +0200

    add project infrastructure files

---
+++ README.rst
@@ -0,0 +1,53 @@
+===================
+BigStash Python SDK
+===================
+---------------------------------
+A Python SDK for the BigStash API
+---------------------------------
+
+Overview
+--------
+
+This is a Python SDK for interacting with the BigStash_ service.
+
+.. _BigStash: http://www.bigstash.co/ 
+
+Installation
+------------
+
+To get the latest stable release from PyPi
+
+.. code-block:: bash
+
+    pip install bigstash
+
+To get the latest commit from GitHub
+
+.. code-block:: bash
+
+    pip install -e git+git://github.com/longaccess/bigstash-python.git#egg=bigstash
+
+
+Usage
+-----
+
+To be documented.
+
+
+Contribute
+----------
+
+If you want to contribute to this project, please perform the following steps
+
+.. code-block:: bash
+
+    # Fork this repository
+    # Clone your fork
+    mkvirtualenv -p python2.7 bigstash
+    make develop
+
+    git co -b feature_branch master
+    # Implement your feature and tests
+    git add . && git commit
+    git push -u origin feature_branch
+    # Send us a pull request for your feature branch
---
+++ setup.py
@@ -0,0 +1,66 @@
+import os
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'BigStash/version.py'
+versioneer.versionfile_build = 'BigStash/version.py'
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'bigstash-'
+from setuptools import setup, find_packages
+
+
+def pep386adapt(version):
+    if version is not None and '-' in version:
+        # adapt git-describe version to be in line with PEP 386
+        parts = version.split('-')
+        parts[-2] = 'post'+parts[-2]
+        version = '.'.join(parts[:-1])
+    return version
+
+install_requires = [
+    'boto>=2.14, <3',
+]
+
+
+dev_requires = [
+    'flake8',
+]
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setup(version=pep386adapt(versioneer.get_version()),
+      name="bigstash",
+      description=read('DESCRIPTION'),
+      long_description=read('README.rst'),
+      url='http://github.com/longaccess/bigstash-python/',
+      license='Apache',
+      packages=find_packages(exclude=['features*', '*.t']),
+      tests_require=['testtools'],
+      test_suite="BigStash.t",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Archiving',
+          'Topic :: Utilities',
+          ],
+      cmdclass=versioneer.get_cmdclass(),
+      install_requires=install_requires,
+      extras_require={
+          'dev': dev_requires,
+          }
+      )
