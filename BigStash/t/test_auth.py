4a45256b614ebf8a8455562b63c1d50ec1521c71
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-02-02 11:59:52 +0200

    add a test class for auth.py

---
+++ BigStash/t/test_auth.py
@@ -0,0 +1,32 @@
+from mock import Mock
+from testtools.matchers import Contains
+from testtools import TestCase
+
+
+class AuthTest(TestCase):
+    def setUp(self):
+        super(AuthTest, self).setUp()
+
+    def tearDown(self):
+        super(AuthTest, self).tearDown()
+
+    def _makeit(self, *args, **kwargs):
+        from BigStash.auth import Auth
+        return Auth(*args, **kwargs)
+
+    def test_auth_class(self):
+        assert self._makeit(self.getUniqueString(),
+                            self.getUniqueString(),
+                            self.getUniqueString())
+
+    def test_do_login(self, stdout):
+        requests = Mock()
+        requests.post.return_value = self.getUniqueString()
+        api_key = self.getUniqueString()
+        api_secret = self.getUniqueString()
+        url = self.getUniqueString()
+
+        auth = self._makeit(api_key, api_secret, url)
+
+        self.assertThat(auth.GetAPIKey(),
+                        Contains('authentication succesfull'))
