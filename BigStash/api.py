d6b7b8085eb0362d805ce42dc5f8f83a2ee7b52e
Sofia Margariti <sofiamargariti@hotmail.com>
2015-02-02 13:46:37 +0200

    Refactor headers and rename classes

--- BigStash/api.py
+++ BigStash/api.py
@@ -2,19 +2,14 @@ import json
 import requests
 import time
 
-from six.moves.urllib.parse import urlparse
-from BigStash import __version__
 from httpsig.requests_auth import HTTPSignatureAuth
+from requests.exceptions import RequestException
+from six.moves.urllib.parse import urlparse
+from .error import BigStashError
+from .auth import headers
 
 
-headers = {
-    'User-agent': 'BigStash Python SDK v{}'.format(__version__),
-    'Accept': 'application/vnd.deepfreeze+json',
-    'Content-Type': 'application/vnd.deepfreeze+json'
-}
-
-
-class API(object):
+class BigStashAPI(object):
 
     def __init__(self, key=None, secret=None, base_url=None):
         self.key = key
@@ -24,7 +19,7 @@ class API(object):
     def _AuthRequest(self, url):
         if self.key and self.secret:
             signature_headers = ['(request-line)', 'date', 'host']
-            host = urlparse(self.base_url + url).netloc
+            host = urlparse(url).netloc
             headers.update({
                 'Host': host,
                 'X-Deepfreeze-Api-Key': self.key,
@@ -36,11 +31,11 @@ class API(object):
                                      algorithm='hmac-sha256',
                                      headers=signature_headers)
 
-    def GetArchiveList(self):
+    def GetArchives(self):
         url = self.base_url + 'archives/'
         auth = self._AuthRequest(url)
         try:
-            req = requests.get(url, auth=auth, headers=headers)
+            req = requests.get(url, auth=self._AuthRequest(url), headers=headers)
             return json.loads(req.content)
         except requests.RequestsException:
             pass
--- BigStash/auth.py
+++ BigStash/auth.py
@@ -1,9 +1,17 @@
 import json
 import requests
+
+from requests.exceptions import RequestException
 from BigStash import __version__
+from .error import BigStashError
 
+headers = {
+    'User-agent': 'BigStash Python SDK v{}'.format(__version__),
+    'Accept': 'application/vnd.deepfreeze+json',
+    'Content-Type': 'application/vnd.deepfreeze+json'
+}
 
-class Auth(object):
+class BigStashAuth(object):
 
     def __init__(self,
                  api_key=None,
@@ -14,11 +22,6 @@ class Auth(object):
         self.base_url = base_url
 
     def GetAPIKey(self, username=None, password=None):
-        headers = {
-            'User-agent': 'BigStash Python SDK v{}'.format(__version__),
-            'Accept': 'application/vnd.deepfreeze+json',
-            'Content-Type': 'application/vnd.deepfreeze+json'
-        }
         url = self.base_url + '/tokens/'
         req = requests.post(url, auth=(username, password),
                             data=json.dumps({"name": "curl.py temp token"}),
