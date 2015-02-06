1e26a1a0989fbd8d8ee5deb2494838e96cdde2b8
Sofia Margariti <sofiamargariti@hotmail.com>
2015-02-06 16:47:50 +0200

    Add BigStashError in __init__.py

--- BigStash/__init__.py
+++ BigStash/__init__.py
@@ -5,5 +5,7 @@ del get_versions
 from .auth import BigStashAuth
 from .api import BigStashAPI
 from .conf import BigStashAPISettings
+from .error import BigStashError
 
-__all__ = ['__version__', 'BigStashAuth', 'BigStashAPI', 'BigStashAPISettings']
+__all__ = ['__version__', 'BigStashAuth', 'BigStashAPI',
+           'BigStashAPISettings', 'BigStashError']
